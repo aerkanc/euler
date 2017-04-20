4ebcfd853ced7136fcc3384c910ccc1bb17322dd
Ahmet Erkan ÇELİK <aerkanc@gmail.com>
2017-04-20 09:09:12 +0300

    syntax fixed

--- problem001.py
+++ problem001.py
@@ -1,10 +1,13 @@
 __author__ = 'Ahmet Erkan ÇELİK'
 from datetime import datetime
+
+
 def main():
     total = 0
     for n in range(3,1000):
         if n % 3 == 0 or n % 5 == 0:
             total+=n
+
     print(total)
 if __name__=="__main__":
     startTime = datetime.now()
