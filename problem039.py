2abd56c2e14cdb68b534e05158b8c35772c2dca6
Ahmet Erkan ÇELİK <aerkanc@gmail.com>
2017-06-09 17:41:04 +0300

    39 solited

--- problem039.py
+++ problem039.py
@@ -11,25 +11,25 @@ startTime = datetime.now()
 # For which value of p ≤ 1000, is the number of solutions maximised?
 
 
-def is_triangle(a, b, c):
+def is_right_angle_triangle(a, b, c):
     sides = [a,b,c]
     sides.sort()
     c = sides[2]
     b = sides[1]
     a = sides[0]
-    if c > a + b or a < c - b: return False
+    if (c > a + b) or (a < c - b) or (c**2 != a**2 + b**2): return False
     return True
 sol = {}
 limit = 1000
 max_p = 0
 max_s = 0
-for p in range(3,limit):
+for p in range(3, limit):
     limit_a = int(p / 3) + 1
-    for a in range(1,limit_a):
-        limit_b = int((p-1)/2) + 1
+    for a in range(1, limit_a):
+        limit_b = int((p - a)/2) + 1
         for b in range(a, limit_b):
             c = p - (a + b)
-            if is_triangle(a, b, c):
+            if is_right_angle_triangle(a, b, c):
                 if p in sol:
                    sol[p] += 1
                 else:
@@ -38,7 +38,6 @@ for p in range(3,limit):
                     max_p = p
                     max_s = sol[p]
 
-# sonuç: 
 print("Result is: ", max_p, max_s)
 print(datetime.now()-startTime)
 
