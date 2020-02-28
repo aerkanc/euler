e561d71be67d6af4a60a3dbe6749d31328987239
aerkanc <aerkanc@gmail.com>
2020-02-28 18:01:43 +0300

    yanlış sonuç üretiyor

--- problem041.py
+++ problem041.py
@@ -16,17 +16,39 @@ result = 1
 def is_prime(x):
     if x < 2:
         return False
-    max = math.floor(math.sqrt(x))+1
-    for n in range(2, max):
+    m = math.floor(math.sqrt(x))+1
+    for n in range(2, m):
         if x % n == 0:
             return False
     else:
         return True
 
-digits = [0,1,2,3,4,5,6,7,8,9]
+
+def get_number(start_index, number_pool):
+    d = number_pool[start_index]
+    new_number_pool = []
+    for n in number_pool:
+        if n != d:
+            new_number_pool.append(n)
+    if len(new_number_pool) == 0:
+        return d
+    start_index +=1
+    if start_index >= len(new_number_pool):
+        start_index = 0
+    return d + get_number(start_index,new_number_pool)
 
 
-print("Result is: ", result)
+def get_prime_pandigital_number():
+    digits = ["9","8","7","6","5","4","3","2","1"]
+    digit_num = len(digits)
+    for dn in range(digit_num, 1, -1):
+        pool = digits.copy()[digit_num-dn:dn]
+        for i in range(0, dn):
+            number = get_number(i, pool)
+            if is_prime(int(number)):
+                return number
+
+print("Result is: ", get_prime_pandigital_number())
 print(datetime.now()-startTime)
 
 
