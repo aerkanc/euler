dd5675f0e6d721c3cf23d9a132cc9efac76c39b7
aerkanc <aerkanc@gmail.com>
2020-03-02 17:33:42 +0300

    solved

--- problem041.py
+++ problem041.py
@@ -16,7 +16,7 @@ result = 1
 def is_prime(x):
     if x < 2:
         return False
-    m = math.floor(math.sqrt(x))+1
+    m = math.floor(math.sqrt(x)) + 1
     for n in range(2, m):
         if x % n == 0:
             return False
@@ -24,32 +24,35 @@ def is_prime(x):
         return True
 
 
-def get_number(start_index, number_pool):
-    d = number_pool[start_index]
-    new_number_pool = []
-    for n in number_pool:
-        if n != d:
-            new_number_pool.append(n)
-    if len(new_number_pool) == 0:
-        return d
-    start_index +=1
-    if start_index >= len(new_number_pool):
-        start_index = 0
-    return d + get_number(start_index,new_number_pool)
+def get_number(number_pool):
+    if len(number_pool) == 1:
+        for d in number_pool:
+            yield d
+    else:
+        for start_index in range(0, len(number_pool)):
+            d = number_pool[start_index]
+            new_number_pool = []
+            for n in number_pool:
+                if n != d:
+                    new_number_pool.append(n)
+            for n in get_number(new_number_pool):
+                if n is not None:
+                    yield d + n
+                start_index += 1
+                if start_index >= len(new_number_pool):
+                    start_index = 0
 
 
 def get_prime_pandigital_number():
-    digits = ["9","8","7","6","5","4","3","2","1"]
+    digits = ["9", "8", "7", "6", "5", "4", "3", "2", "1"]
     digit_num = len(digits)
     for dn in range(digit_num, 1, -1):
-        pool = digits.copy()[digit_num-dn:dn]
-        for i in range(0, dn):
-            number = get_number(i, pool)
+        pool = digits[digit_num-dn:]
+        for number in get_number(pool):
+            print("Generated Number: %s" % (number))
             if is_prime(int(number)):
                 return number
 
+
 print("Result is: ", get_prime_pandigital_number())
-print(datetime.now()-startTime)
-
-
-
+print(datetime.now() - startTime)
