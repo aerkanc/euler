c80d67e70ff6c1e06c3454cbff73c8de6bec7fe6
aerkanc <aerkanc@gmail.com>
2020-10-03 15:41:32 +0300

    solved

--- problem047.py
+++ problem047.py
@@ -19,9 +19,43 @@ import math
 
 startTime = datetime.now()
 result = 0
+primes = []
 
+
+def is_prime(x):
+    if x in primes:
+        return True
+
+    if x < 2:
+        return False
+    m = math.floor(math.sqrt(x)) + 1
+    for n in range(2, m):
+        if x % n == 0:
+            return False
+    else:
+        primes.append(x)
+        return True
+
+
+consecutive_numbers = []
+n = 999
+while len(consecutive_numbers) < 4:
+    n += 1
+    prime_factors = []
+    for f in range(2, n):
+        if n % f == 0 and is_prime(f) and f not in prime_factors:
+            prime_factors.append(f)
+            if len(prime_factors) > 4:
+                continue
+    if len(prime_factors) == 4 and (len(consecutive_numbers) == 0 or consecutive_numbers[-1] + 1 == n):
+        consecutive_numbers.append(n)
+        print("%d is consecutive number as four distinct prime factors [%s]" % (
+            n, ",".join(str(e) for e in prime_factors)))
+    else:
+        consecutive_numbers = []
+
+result = consecutive_numbers[0]
 
 print("Result is: %s" % (result))
 print(datetime.now() - startTime)
 exit()
-
