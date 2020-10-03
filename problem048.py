714fb83deef3fbfe381308a59dc8281a183dda37
aerkanc <aerkanc@gmail.com>
2020-10-03 15:42:38 +0300

    prepare  48

---
+++ problem048.py
@@ -0,0 +1,17 @@
+__author__ = 'Ahmet Erkan ÇELİK'
+
+from datetime import datetime
+import math
+
+# The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+#
+# Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+
+
+startTime = datetime.now()
+result = 0
+
+
+print("Result is: %s" % (result))
+print(datetime.now() - startTime)
+exit()
